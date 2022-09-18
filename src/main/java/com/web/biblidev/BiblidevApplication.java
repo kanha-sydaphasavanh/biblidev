6711a3d1f5fa3d8c32dab1a88627b00b1b02d2d8
Kanha <k.sydaphasavanh@gmail.com>
2022-09-18 16:29:14 +0200

    Initial commit

---
+++ src/main/java/com/web/biblidev/BiblidevApplication.java
@@ -0,0 +1,13 @@
+package com.web.biblidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblidevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblidevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/biblidev/BiblidevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.biblidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblidevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
